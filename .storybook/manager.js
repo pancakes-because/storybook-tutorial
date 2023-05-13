cd204638973b3038c9740f9aee1a71f708708e7e
Diya O'Brien <dbrien.diya@gmail.com>
2023-05-13 10:20:09 -0500

    created a custom theme

---
+++ .storybook/manager.js
@@ -0,0 +1,21 @@
+// for themes, must quit and restart storybook server to see changes
+// to quit, do "control c". to start again, do "npm run storybook"
+
+// for using "light" or "dark" theming with manager.js 
+
+// import { addons } from '@storybook/manager-api';
+// import { themes } from '@storybook/theming';
+
+// addons.setConfig({
+//   theme: themes.dark,
+// });
+
+// for creating a custom theme (quick start)
+// partners with YourTheme.js to make this work 
+
+import { addons } from '@storybook/manager-api';
+import yourTheme from './YourTheme';
+
+addons.setConfig({
+  theme: yourTheme,
+}); 
