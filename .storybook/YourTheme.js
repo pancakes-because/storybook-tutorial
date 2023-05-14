c0b5ec93287c5e4a723047913449826b8622295e
Diya O'Brien <dbrien.diya@gmail.com>
2023-05-14 13:19:40 -0500

    provided a mock data store for our stories to pull from

--- .storybook/YourTheme.js
+++ .storybook/YourTheme.js
@@ -15,6 +15,50 @@
 // });
 
 // for creating a custom theme (complex)
+// this is the custom theme I made that includes a gif and a green color palette
+
+// import { create } from '@storybook/theming/create';
+
+// export default create({
+//   base: 'light',
+//   // Typography
+//   fontBase: '"Open Sans", sans-serif',
+//   fontCode: 'monospace',
+
+//   brandTitle: 'My custom Storybook',
+//   brandUrl: 'https://example.com',
+//   // brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+//   brandImage: 'https://media.giphy.com/media/SsTcO55LJDBsI/giphy.gif',
+//   brandTarget: '_self',
+
+//   //
+//   colorPrimary: '#3A10E5',
+//   colorSecondary: '#13AA6A',
+
+//   // UI
+//   appBg: '#F4FBF8',
+//   appContentBg: '#F4FBF8',
+//   appBorderColor: '#13AA6A',
+//   appBorderRadius: 4,
+
+//   // Text colors
+//   textColor: '#000000',
+//   textInverseColor: '#ffffff',
+
+//   // Toolbar default and active colors
+//   barTextColor: '#000000',
+//   barSelectedColor: '#13AA6A',
+//   barBg: '#F4FBF8',
+
+//   // Form colors
+//   inputBg: '#ffffff',
+//   inputBorder: '#10162F',
+//   inputTextColor: '000000',
+//   inputBorderRadius: 2,
+// });
+
+// for creating a custom theme
+// this is storybooks default custom theme
 
 import { create } from '@storybook/theming/create';
 
@@ -26,32 +70,31 @@ export default create({
 
   brandTitle: 'My custom Storybook',
   brandUrl: 'https://example.com',
-  // brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
-  brandImage: 'https://media.giphy.com/media/SsTcO55LJDBsI/giphy.gif',
+  brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
   brandTarget: '_self',
 
   //
   colorPrimary: '#3A10E5',
-  colorSecondary: '#13AA6A',
+  colorSecondary: '#585C6D',
 
   // UI
-  appBg: '#F4FBF8',
-  appContentBg: '#F4FBF8',
-  appBorderColor: '#13AA6A',
+  appBg: '#ffffff',
+  appContentBg: '#ffffff',
+  appBorderColor: '#585C6D',
   appBorderRadius: 4,
 
   // Text colors
-  textColor: '#000000',
+  textColor: '#10162F',
   textInverseColor: '#ffffff',
 
   // Toolbar default and active colors
-  barTextColor: '#000000',
-  barSelectedColor: '#13AA6A',
-  barBg: '#F4FBF8',
+  barTextColor: '#9E9E9E',
+  barSelectedColor: '#585C6D',
+  barBg: '#ffffff',
 
   // Form colors
   inputBg: '#ffffff',
   inputBorder: '#10162F',
-  inputTextColor: '000000',
+  inputTextColor: '#10162F',
   inputBorderRadius: 2,
 });
